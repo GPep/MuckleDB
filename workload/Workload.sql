ea1a75d094f54044e3071c2a7c27eaab2c4315f0
Glenn Pepper <glenn@darkmink.com>
2019-01-17 09:44:25 +0000

    new

---
+++ workload/Workload.sql
@@ -0,0 +1,17 @@
+EXECUTE sp_RandomTransactionBusiness @count = 5
+
+EXECUTE sp_RandomTransactionSavings @count = 5
+
+EXECUTE sp_RandomTransactionCurrent @count = 50
+
+
+EXECUTE dbo.sp_createfemaleCustomer @count = 1
+
+
+EXECUTE dbo.sp_createMaleCustomer @count = 1
+
+
+EXECUTE dbo.sp_createBusinessCustomer @Count = 1
+
+
+
